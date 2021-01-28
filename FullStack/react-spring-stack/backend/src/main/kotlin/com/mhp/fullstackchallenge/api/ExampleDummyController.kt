0af2920aaf81508474bf04bbe44b5bee866cf4ec
Christian Dräger <christian.draeger1@gmail.com>
2021-01-28 12:01:27 +0100

    add example frontend-to-backend communication

--- FullStack/react-spring-stack/backend/src/main/kotlin/com/mhp/fullstackchallenge/api/ExampleDummyController.kt
+++ FullStack/react-spring-stack/backend/src/main/kotlin/com/mhp/fullstackchallenge/api/ExampleDummyController.kt
@@ -1,13 +1,15 @@
 package com.mhp.fullstackchallenge.api
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api")
 @RestController
+@RequestMapping("/api")
 class ExampleDummyController {
 
+    @CrossOrigin
     @GetMapping("/hello")
     fun getHelloWorld() = " hello world from the backend"
 }
--- FullStack/react-spring-stack/frontend/src/App.js
+++ FullStack/react-spring-stack/frontend/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const  [helloMessage,setHelloMessage]= useState("")
+
+  useEffect(() =>
+      fetch("http://localhost:8080/api/hello")
+          .then(res => res.text())
+          .then(res => setHelloMessage(res))
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{helloMessage}</p>
       </header>
     </div>
   );
