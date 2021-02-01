ac34696a5ce99a2b3ab76d650a29be986b4a4f36
Christian Dräger <christian.draeger1@gmail.com>
2021-02-01 09:52:46 +0100

    Revert "add example frontend-to-backend communication"
    This reverts commit 0af2920aaf81508474bf04bbe44b5bee866cf4ec.

--- FullStack/react-spring-stack/backend/src/main/kotlin/com/mhp/fullstackchallenge/api/ExampleDummyController.kt
+++ FullStack/react-spring-stack/backend/src/main/kotlin/com/mhp/fullstackchallenge/api/ExampleDummyController.kt
@@ -1,15 +1,13 @@
 package com.mhp.fullstackchallenge.api
 
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
 @RequestMapping("/api")
+@RestController
 class ExampleDummyController {
 
-    @CrossOrigin
     @GetMapping("/hello")
     fun getHelloWorld() = " hello world from the backend"
 }
--- FullStack/react-spring-stack/frontend/src/App.js
+++ FullStack/react-spring-stack/frontend/src/App.js
@@ -1,19 +1,22 @@
-import { useState, useEffect } from "react";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const  [helloMessage,setHelloMessage]= useState("")
-
-  useEffect(() =>
-      fetch("http://localhost:8080/api/hello")
-          .then(res => res.text())
-          .then(res => setHelloMessage(res))
-  );
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>{helloMessage}</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
